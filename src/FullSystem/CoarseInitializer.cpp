5b93906ea8b5266ba99eaa6ad9fed885a83179a2
Israel Shirk <israelshirk@gmail.com>
2017-07-28 17:23:17 -0600

    Formatted with style Google, tabs

--- src/FullSystem/CoarseInitializer.cpp
+++ src/FullSystem/CoarseInitializer.cpp
@@ -764,8 +764,8 @@ void CoarseInitializer::setFirst(CalibHessian* HCalib,
 						sumGrad2 += absgrad;
 					}
 
-					//				float gth =
-					//setting_outlierTH *
+					//				float gth
+					//=  setting_outlierTH *
 					//(sqrtf(sumGrad2)+setting_outlierTHSumComponent);
 					//				pl[nl].outlierTH
 					//= patternNum*gth*gth;
